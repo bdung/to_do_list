4176a86eb2f8d2e458ab45c8ee1d163a106fc261
bdung <bdung0855@gmail.com>
2024-05-23 19:19:48 +0700

    start project

---
+++ lib/model/task_model.dart
@@ -0,0 +1,56 @@
+import 'dart:ffi';
+
+class Task {
+  int? id;
+  String? title;
+  String? shortDescription;
+  String? dateCreated;
+  String? timeCreated;
+  String? longDescription;
+  String? repeatTask;
+  String? reminder;
+  String? color;
+  bool? isCompleted;
+  bool? state;
+  Task(
+      {this.id,
+      this.title,
+      this.shortDescription,
+      this.dateCreated,
+      this.timeCreated,
+      this.longDescription,
+      this.repeatTask,
+      this.reminder,
+      this.color});
+
+  Task.fromJson(Map<String, dynamic> json) {
+    id = json["id"];
+    title = json["title"];
+    shortDescription = json["shortDescription"];
+    dateCreated = json["dateCreated"];
+    timeCreated = json["timeCreated"];
+    longDescription = json["longDescription"];
+    repeatTask = json["repeatTask"];
+    reminder = json["reminder"];
+    color = json["color"];
+    isCompleted = json["isCompleted"];
+    state = json["state"];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['id'] = id;
+    data['title'] = title;
+    data['shortDescription'] = shortDescription;
+    data['dateCreated'] = dateCreated;
+    data['longDescription'] = longDescription;
+    data['repeatTask'] = repeatTask;
+    data['timeCreated'] = timeCreated;
+    data['reminder'] = reminder;
+    data['color'] = color;
+    data['isCompleted'] = isCompleted;
+    data['state'] = state;
+
+    return data;
+  }
+}
---
+++ lib/utils/item_task.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_list/model/task_model.dart';
+
+class ItemTaskToDo extends StatelessWidget {
+  const ItemTaskToDo({super.key, required this.task});
+
+  final Task task;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

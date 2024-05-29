2b068cfe2b020e2edebda2ad99416436ab353bcf
bdung <bdung0855@gmail.com>
2024-05-29 21:32:08 +0700

    cm

--- lib/screens/components/calendar_weeks.dart
+++ lib/screens/components/calendar_weeks.dart
@@ -1,11 +1,8 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_calendar_week/flutter_calendar_week.dart';
 import 'package:intl/intl.dart';
 
 import '../../constant.dart';
-
 
 class CalendarWeeks extends StatefulWidget {
   const CalendarWeeks({super.key});
@@ -18,7 +15,7 @@ class _CalendarWeeksState extends State<CalendarWeeks> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(vertical: kDefaultPadding),
+        margin: EdgeInsets.symmetric(vertical: kDefaultPadding),
         decoration: BoxDecoration(
           color: kTextWhiteColor,
           borderRadius: BorderRadius.circular(10),
@@ -31,68 +28,69 @@ class _CalendarWeeksState extends State<CalendarWeeks> {
             ),
           ],
         ),
-      child: CalendarWeek(
-        controller: CalendarWeekController(),
-        height: 110,
-        showMonth: true,
-        minDate: DateTime.now().add(
-          Duration(days: -365),
-        ),
-        maxDate: DateTime.now().add(
-          Duration(days: 365),
-        ),
-
-        onDatePressed: (DateTime datetime) {
-          print(datetime);
-        },
-        monthViewBuilder: (DateTime time) => Align(
-          alignment: FractionalOffset.center,
-          child: Container(
-              margin: const EdgeInsets.symmetric(vertical: 4),
-              child: Text(
-                DateFormat.yMMMM().format(time),
-                overflow: TextOverflow.ellipsis,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.blue, fontWeight: FontWeight.w600),
-              )),
-        ),
-
-        /*Custom SATURDAY, SUNDAY*/
-        weekendsStyle: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
-
-        /*Custom MONDAY... FRIDAY*/
-        dayOfWeekStyle: TextStyle(fontSize: 10, color: kTextColorBold, fontWeight: FontWeight.bold),
-        dayShapeBorder: BoxShape.circle,
-
-        /*Custom pressing*/
-        pressedDateBackgroundColor: Colors.blue.withOpacity(0.5),
-        pressedDateStyle: TextStyle(fontWeight: FontWeight.bold, color: kTextWhiteColor),
-
-        /*Custom today*/
-        todayBackgroundColor: Colors.lightBlue,
-        todayDateStyle: TextStyle(color: kTextWhiteColor, fontWeight: FontWeight.bold),
-        decorations: [
-
-          DecorationItem(
-              decorationAlignment: FractionalOffset.bottomCenter,
-              date: DateTime.now(),
-              decoration: Container(
-                // margin: EdgeInsets.only(top: 20),
-                child: Icon(
-                  Icons.horizontal_rule,
-                  color: Colors.blue,
-                  // size: ,
-                ),
-              )
-              // decoration: Icon(
-              //   Icons.today,
-              //   color: Colors.blue,
-              // )
+        child: CalendarWeek(
+          controller: CalendarWeekController(),
+          height: 110,
+          showMonth: true,
+          minDate: DateTime.now().add(
+            Duration(days: -365),
+          ),
+          maxDate: DateTime.now().add(
+            Duration(days: 365),
           ),
 
-        ],
-      )
-    );
+          onDatePressed: (DateTime datetime) {
+            print(datetime);
+          },
+          // monthViewBuilder: (DateTime time) => Align(
+          //   alignment: FractionalOffset.center,
+          //   child: Container(
+          //       margin: const EdgeInsets.symmetric(vertical: 4),
+          //       child: Text(
+          //         DateFormat.yMMMM().format(time),
+          //         overflow: TextOverflow.ellipsis,
+          //         textAlign: TextAlign.center,
+          //         style: TextStyle(
+          //             color: Colors.blue, fontWeight: FontWeight.w600),
+          //       )),
+          // ),
+
+          /*Custom SATURDAY, SUNDAY*/
+          weekendsStyle:
+              TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+
+          /*Custom MONDAY... FRIDAY*/
+          dayOfWeekStyle: TextStyle(
+              fontSize: 10, color: kTextColorBold, fontWeight: FontWeight.bold),
+          // dayShapeBorder: BoxShape.circle,
+
+          /*Custom pressing*/
+          pressedDateBackgroundColor: Colors.blue.withOpacity(0.5),
+          pressedDateStyle:
+              TextStyle(fontWeight: FontWeight.bold, color: kTextWhiteColor),
+
+          /*Custom today*/
+          todayBackgroundColor: Colors.lightBlue,
+          todayDateStyle:
+              TextStyle(color: kTextWhiteColor, fontWeight: FontWeight.bold),
+          decorations: [
+            DecorationItem(
+                decorationAlignment: FractionalOffset.bottomCenter,
+                date: DateTime.now(),
+                decoration: Container(
+                  // margin: EdgeInsets.only(top: 20),
+                  child: Icon(
+                    Icons.horizontal_rule,
+                    color: Colors.blue,
+                    // size: ,
+                  ),
+                )
+                // decoration: Icon(
+                //   Icons.today,
+                //   color: Colors.blue,
+                // )
+                ),
+          ],
+        ));
   }
 }
